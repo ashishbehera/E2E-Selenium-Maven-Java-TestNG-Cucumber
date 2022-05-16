717c487abb494786ba7a61954869f35dcfc3e5ba
ashish.behera <abehera2@sapient.com>
2022-05-16 22:45:55 +0100

    Updated Test cases

--- src/test/java/cucumberOptions/TestRunnerCucumber.java
+++ src/test/java/cucumberOptions/TestRunnerCucumber.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 //@RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/features"
 ,glue = "stepDefinitions"
-,tags ="@smokeTestDemo"
+,tags ="@smokeTest"
 ,   plugin = {"pretty",
         "html:target/cucumber-reports/cucumber.html",
         "json:target/cucumber-reports/cucumber.json"
